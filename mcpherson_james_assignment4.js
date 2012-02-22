a6479c257b497b8d303fb3183fbb720b4e83a361
James McPherson <jemcphe@fullsail.edu>
2012-02-22 15:49:48 -0600

    testEmailAddress Function
    Added testEmailAddress Function to test for a valid email address.
    Very similar to the phone number w/ regular expression to handle email.

--- mcpherson_james_assignment4.js
+++ mcpherson_james_assignment4.js
@@ -1,16 +1,31 @@
 alert("JavaScript works!");
 
-//var validNumber = new RegExp("/ ^ \d{3}- \d{3}- \d{4}$/") ;
-
+/***********************String Library***********************/
+// Test for correct phone number input
 var testPhoneNumber = function (phoneNumber) {
-	var validNumber =   /^\d{3}-\d{3}-\d{4}$/;
+	var validNumber =   /^\d{3}-\d{3}-\d{4}$/; // regular expression tests for ###-###-####
 	if(!validNumber.exec(phoneNumber)) {
 		console.log("Sorry, " + phoneNumber + " is an invalid phone number");
-		return false;
+		return false; 
 	} else {
 		console.log(phoneNumber + " is a valid phone number!");
+		return true; 
+		// MAKEUP: return Boolean, deliverable 3, deliverable 2
+	}
+};
+
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
+// Test for Correct Email
+var testEmailAddress = function (emailAddress) {
+	var validEmail = /^([A-Za-z0-9]+([_.]+[A-Za-z0-9]+)*@[A-Za-z0-9]*(.){1}[a-zA-Z]{2,4})$/;
+	if(!validEmail.exec(emailAddress)) {
+		console.log("Sorry, " + emailAddress + " is an invalid Email Address");
+		return false; 
+	} else {
+		console.log(emailAddress + " is a valid Email Address!");
 		return true;
 	}
 };
 
 testPhoneNumber("123-456-7890");
+testEmailAddress("jemcphe11@gmail.com");
