4fcfe2fa3835644c0ecaedf40faddaea4f34a071
James McPherson <jemcphe@fullsail.edu>
2012-02-22 16:32:51 -0600

    testURL Function
    testURL checks whether the URL entered is in a correct format.

--- mcpherson_james_assignment4.js
+++ mcpherson_james_assignment4.js
@@ -17,7 +17,7 @@ var testPhoneNumber = function (phoneNumber) {
 // Does a string follow an aaa@bbb.ccc pattern like an email address?
 // Test for Correct Email
 var testEmailAddress = function (emailAddress) {
-	var validEmail = /^([A-Za-z0-9]+([_.]+[A-Za-z0-9]+)*@[A-Za-z0-9]*(.){1}[a-zA-Z]{2,4})$/;
+	var validEmail = /^([A-Za-z0-9]+([_.]+[A-Za-z0-9]+)*@[A-Za-z0-9]*(.){1}[a-zA-Z]{2,4})$/; // Regular Expression
 	if(!validEmail.exec(emailAddress)) {
 		console.log("Sorry, " + emailAddress + " is an invalid Email Address");
 		return false; 
@@ -27,5 +27,20 @@ var testEmailAddress = function (emailAddress) {
 	}
 };
 
+// Is the string a URL?
+// Test for correct URL input
+var testURL = function(webSite) {
+	var validURL = /^(http[s]?:\/\/)?(www\.)?[a-zA-Z0-9\.]+(.){1}[a-z]{3}$/;
+	if(!validURL.exec(webSite)) {
+		console.log("Sorry, " + webSite + " is an invalid URL");
+		return false; 
+	} else {
+		console.log(webSite + " is a valid URL!");
+		return true;
+	}
+};
+
+
 testPhoneNumber("123-456-7890");
 testEmailAddress("jemcphe11@gmail.com");
+testURL("http://www.JamesIsAwesome.com");
